88cf02191b3e39a92d3ef674799456d55a2967b2
Horsfeldt <nikolashorsfeldt@gmail.com>
2026-08-31 10:30:31 +0200

    Js test

---
+++ javascript/index.js
@@ -0,0 +1,5 @@
+console.log("Nikolas");
+//skriver "nikolas" i consolen
+
+document.body.innerHTML = "<h1>felixboy is homoboy</h1>";
+//skriver "felixboy is homoboy" på hjemmesiden og fjerner alt andet indhold på hjemmesiden
---
+++ javascript/variabler.js
@@ -0,0 +1,4 @@
+let x = 2;
+let y = 3;
+
+document.body.innerHTML = "<h1> 2 + 3 = " + (x + y) + "</h1>";
